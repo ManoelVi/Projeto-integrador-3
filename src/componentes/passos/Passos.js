88aa3aa5c7dbfb7bd139afeea5789f046f01ff7b
RaquelATeixeira <85255156+RaquelATeixeira@users.noreply.github.com>
2023-03-08 20:11:13 -0300

    steps

--- src/componentes/passos/Passos.js
+++ src/componentes/passos/Passos.js
@@ -5,6 +5,25 @@ import cartao from '../../img/cartao.PNG'
 
 export default function Passos() {
   return (
-   <div></div>
+    <div className='steps'>
+      <div className='images'>
+        <img src={papel}></img>
+        <img src={homem}></img>
+        <img src={cartao}></img>
+      </div>
+      <div className='barra'>
+        <hr></hr>
+      </div>
+      <div className='bolinhas'>
+        <a>1</a>
+        <a>2</a>
+        <a>3</a>
+      </div>
+      <div className='textos'>
+        <p>Escolha dos serviços mediante a conferência do catálogo</p>
+        <p>Escolha dos serviços mediante a conferência do catálogo</p>
+        <p>Aguardar o contato pelo e-mail/whatsapp</p>
+      </div>
+    </div>
   )
 }
