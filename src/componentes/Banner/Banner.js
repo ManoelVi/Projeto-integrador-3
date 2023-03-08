4e62b38b807363eb58a61abafec214b29a125b67
RaquelATeixeira <85255156+RaquelATeixeira@users.noreply.github.com>
2023-03-08 16:38:52 -0300

    refactor front page

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,17 +1,17 @@
 import "./Banner.css";
-import minhaImagem from '../../img/Capturar.PNG'
+import minhaImagem from '../../img/logo.png'
 
 export const Banner = () => {
   return (
     <header className="banner">
-      <div className="Lista">
+      <div className="lista">
       <img src={minhaImagem} id="imges"/>
         <ul>
           <li className="elementos">Home</li>
           <li className="elementos">Serviços</li>
           <li className="elementos">Solicitação</li>
 
-          <button className="login">Login</button>
+          <a href="https" className="login">Login</a>
         </ul>
       </div>
     </header>
--- src/componentes/sobreNos/sobreNos.js
+++ src/componentes/sobreNos/sobreNos.js
@@ -1,14 +1,16 @@
 import './sobre.css'
 import imagem from '../../img/fale.PNG'
 
-export default function(){
-    return(
-       <div className='fale'>
-            <img src={imagem}/>
-            <h3 id="titulo">Sobre nós</h3>
-            <p id="descricao">Olá, meu nome é Giulia Lanzelotte e sou uma uma profissional de estética e cosmetologia formada pelo Centro Universitário Senac.</p>
-            <p id="descricao2"> Ofereço serviços de estética como limpeza de pele, drenagem linfática, massagem relaxanto e entre outros.</p>
-            <button>Fale conosco</button>
-       </div>
+export default function sobreNos() {
+    return (
+        <div className='faleConosco'>
+            <img className="img" src={imagem} />
+            <div className='texto'>
+                <h3 id="titulo">Sobre nós</h3>
+                <p id="descricao">Olá, meu nome é Giulia Lanzelotte e sou uma uma profissional de estética e cosmetologia formada pelo Centro Universitário Senac.</p>
+                <p id="descricao2"> Ofereço serviços de estética como limpeza de pele, drenagem linfática, massagem relaxanto e entre outros.</p>
+                <a className="vejaServicos" href="https">Veja meus serviços</a>
+            </div>
+        </div>
     )
 }
