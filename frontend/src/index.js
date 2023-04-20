b825888154eeb6a718c5d19b376890160108a530
Raquel A Teixeira <raquelat@ciand.com>
2023-04-20 18:09:13 -0300

    integrated create admin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './assets/css/reset.css';
-import './assets/css/root.css';
-import './assets/css/styles.css';
-import LandingPage from './pages/landingPage';
-import FormReq from './pages/form/form';
-import OrderStatus from './pages/orderStatus';
-import AdminForm from './pages/adminLogin';
-import Servicos from './pages/servicos';
-import RequestsList from './pages/requests';
-import HistoryList from './pages/history';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./assets/css/reset.css";
+import "./assets/css/root.css";
+import "./assets/css/styles.css";
+import LandingPage from "./pages/landingPage";
+import FormReq from "./pages/form/form";
+import OrderStatus from "./pages/orderStatus";
+import AdminForm from "./pages/adminLogin";
+import Servicos from "./pages/servicos";
+import RequestsList from "./pages/requests";
+import HistoryList from "./pages/history";
+import AdminFormCadastro from "./pages/form/adminCreateForm";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LandingPage/>} />
-        <Route path='/requisicao' element={<FormReq />} />
-        <Route path='/pedido-finalizado' element={<OrderStatus />} />
-        <Route path='/login' element={<AdminForm />} />
-        <Route path='/admin/register' element={<AdminForm />} />
-        <Route path='/servicos' element={<Servicos />} />
-        <Route path='/admin/requests' element={<RequestsList />} />
-        <Route path='/admin/history' element={<HistoryList />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/requisicao" element={<FormReq />} />
+        <Route path="/pedido-finalizado" element={<OrderStatus />} />
+        <Route path="/login" element={<AdminForm />} />
+        <Route path="/admin/register" element={<AdminForm />} />
+        <Route path="/servicos" element={<Servicos />} />
+        <Route path="/admin/requests" element={<RequestsList />} />
+        <Route path="/admin/history" element={<HistoryList />} />
+        <Route path="/cadastro" element={<AdminFormCadastro />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ frontend/src/pages/form/adminCreateForm/index.js
@@ -0,0 +1,89 @@
+import { Navigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import "./index.css";
+import Header from "../../../components/Header";
+import Footer from "../../../components/Footer";
+
+export default function AdminFormCadastro() {
+  const [redirect, setRedirect] = useState(false);
+  const [error, setError] = useState(null);
+  const [req, setReq] = useState({
+    userName: "",
+    password: "",
+  });
+  const handleInputChangeReq = (event) => {
+    const { name, value } = event.target;
+    setReq({
+      ...req,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`http://localhost:8080/admin/create`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req),
+    }).then((response) => {
+      if (response.ok) {
+        setRedirect(true);
+        return response.json();
+      } else {
+        setError("Este usuário já existe, tente novamente.");
+        throw new Error("Failed to login as admin");
+      }
+    });
+  };
+  if (redirect === true) {
+    return <Navigate to="/menu" />;
+  }
+
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="login-form">
+          <h2>
+            {window.location.pathname.includes("login") ? "Login" : "Cadastro"}{" "}
+            Administrador(a)
+          </h2>
+          {error && <p className="error-message">{error}</p>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                required
+                size="lg"
+                type="text"
+                name="userName"
+                value={req.userName}
+                onChange={handleInputChangeReq}
+                placeholder="Nome de usuário"
+                className="login-input"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                required
+                size="lg"
+                type="password"
+                name="password"
+                value={req.password}
+                onChange={handleInputChangeReq}
+                placeholder="********"
+                className="login-input"
+              />
+            </Form.Group>
+            <div className="button">
+              <Button variant="primary" type="submit">
+                Create
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
