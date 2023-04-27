fa66c3eb9e54f7bf49443b5b8ec8107d73debb64
EndriOliveira <endriojesus76@gmail.com>
2023-04-27 11:41:49 -0300

    Logout de administrador

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -5,25 +5,49 @@ import './index425px.css';
 import './index1024px.css';
 
 export default function Header() {
-//  if(window.location.pathname.includes('admin') && !window.location.pathname.includes('register')){
-  //  const isLogged = localStorage.getItem('isLogged');
-  // if(!isLogged){
-   //   window.location.href = '/login';
-   // }
- // }
-  return (
-    <>
-      <header id="header">
-        <div className="list">
-          <img src={logo} id="header-logo" alt="Lanzelotti Beauty Logo" />
-          <ul>
-            <li><Link to="/" className="elementos">Home</Link></li>
-            <li><Link to="/servicos" className="elementos">Serviços</Link></li>
-            <li><Link to="/requisicao" className="elementos">Solicitação</Link></li>
-            <li><Link to="/login" className="elementos login">Sou administrador</Link></li>
-          </ul>
-        </div>
-      </header>
-    </>
-  )
+  if (window.location.pathname.includes('admin') && !window.location.pathname.includes('register')) {
+    const isLogged = localStorage.getItem('isLogged');
+    if (!isLogged) {
+      window.location.href = '/login';
+    }
+  }
+
+  if(localStorage.getItem('isLogged')) {
+    return (
+      <>
+        <header id="header">
+          <div className="list">
+            <img src={logo} id="header-logo" alt="Lanzelotti Beauty Logo" />
+            <ul>
+              <li><Link to="/" className="elementos">Home</Link></li>
+              <li><Link to="/servicos" className="elementos">Serviços</Link></li>
+              <li><Link to="/requisicao" className="elementos">Solicitação</Link></li>
+              <li><span className="elementos login" onClick={() => {
+                localStorage.removeItem('isLogged')
+                window.location.href = '/'
+              }}>Sair</span></li>
+              <li><Link to="/admin/history" className="elementos login">Área do administrador</Link></li>
+
+            </ul>
+          </div>
+        </header>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <header id="header">
+          <div className="list">
+            <img src={logo} id="header-logo" alt="Lanzelotti Beauty Logo" />
+            <ul>
+              <li><Link to="/" className="elementos">Home</Link></li>
+              <li><Link to="/servicos" className="elementos">Serviços</Link></li>
+              <li><Link to="/requisicao" className="elementos">Solicitação</Link></li>
+              <li><Link to="/login" className="elementos login">Sou administrador</Link></li>
+            </ul>
+          </div>
+        </header>
+      </>
+    )
+  }
 }
