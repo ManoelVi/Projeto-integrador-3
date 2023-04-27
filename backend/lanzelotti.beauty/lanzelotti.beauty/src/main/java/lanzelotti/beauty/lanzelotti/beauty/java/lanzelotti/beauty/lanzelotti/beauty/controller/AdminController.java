25631223778982b667c8250538300e20e159f58e
RAQUEL APARECIDA TEIXEIRA <raquel.ateixeira3@senacsp.edu.br>
2023-04-27 19:22:57 -0300

    delete

--- backend/lanzelotti.beauty/lanzelotti.beauty/src/main/java/lanzelotti/beauty/lanzelotti/beauty/java/lanzelotti/beauty/lanzelotti/beauty/controller/AdminController.java
+++ backend/lanzelotti.beauty/lanzelotti.beauty/src/main/java/lanzelotti/beauty/lanzelotti/beauty/java/lanzelotti/beauty/lanzelotti/beauty/controller/AdminController.java
@@ -4,9 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,5 +50,20 @@ public class AdminController {
     @GetMapping("/users")
     public List<AdminUser> login() {
         return adminRepository.findAll();
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public ResponseEntity<AdminUser> delete(@PathVariable Long id) {
+        Optional<AdminUser> optionalRequest = adminRepository.findById(id);
+
+        if (optionalRequest.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        AdminUser user = optionalRequest.get();
+
+        adminRepository.delete(user);
+        
+        return ResponseEntity.ok(user);
     }
 }
